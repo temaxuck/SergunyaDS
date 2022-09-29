e6b05b668c6372cdd8818e76c4da05f43e555dac
temaxuck <gorropand@gmail.com>
2022-09-29 11:30:31 +0700

    Added 2 template api recources: Service, ServiceList:

--- api-server-flask/api/routes/v1.py
+++ api-server-flask/api/routes/v1.py
@@ -1,13 +1,27 @@
 from flask_restful import Resource, abort
 from flask import Blueprint
+from api import api
 
 apiv1bp = Blueprint('api', __name__)
 
 
 class Service(Resource):
-    def get(self, id):
+    def get(self, service_id):
         return ({
             'name': 'bebe',
             'status': 0,
             'cover': 'cover.png'
         }, 200)
+
+
+class ServiceList(Resource):
+    def get(self):
+        return ({
+            'name': 'bebe',
+            'status': 0,
+            'cover': 'cover.png'
+        }, 200)
+
+
+api.add_resource(Service, '/service/<int:service_id>')
+api.add_resource(ServiceList, '/services')
